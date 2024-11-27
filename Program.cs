268bc5a41dcac58d3876364797df63f805111b61
bass <msileha@mail.ru>
2024-11-27 16:14:14 +0300

    create full code

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace calculator;
+      
+class Programm
+{
+    static void print(string qwe)
+    {
+        Console.WriteLine(qwe);
+    }
+
+    static void Main(string[] args)
+    {
+        print("Введите первое число: ");
+        int number_1 = Convert.ToInt32(Console.ReadLine());
+
+        print("Введите второе число: ");
+        int number_2 = Convert.ToInt32(Console.ReadLine());
+
+        int summ = number_1 + number_2;
+        print("Сумма = " + summ);
+    }
+}
